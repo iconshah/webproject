796adc26d95fd03f9137106a2e6983b19de8e4ca
Roman Shrestha <stha.roman20@gmail.com>
2024-04-01 21:21:33 +0300

    modified for user register and login post

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const { pool } = require('../helpers/db.js');
-const router = express.Router();
+const { pool } = require('../helpers/db.js'); // Import the pool object from db.js
+const bcrypt = require('bcrypt'); // Import bcrypt for password hashing
 
-// Routes
-
-router.get('/', (req, res) => {
-    res.render('index.ejs');
-});
-
-
-// GET /users/register
-router.get('/users/register', checkAuthenticated, (req, res) => {
-    res.render('register.ejs');
-});
-
-// POST /users/register
-router.post('/users/register', async (req, res) => {
+register = async (req, res) => {
     // Extract the name, email, password, and password2 from the request body
     const { name, email, password, password2 } = req.body;
     // Create an empty array to store any errors
@@ -56,42 +40,6 @@ router.post('/users/register', async (req, res) => {
         req.flash('error_msg', 'Failed to register user.');
         res.redirect('/users/register');
     }
-});
-
-router.get('/users/login', checkAuthenticated, (req, res) => {
-    res.render('login.ejs');
-});
-
-router.post('/users/login', passport.authenticate('local', {
-    successRedirect: '/users/dashboard',
-    failureRedirect: '/users/login',
-    failureFlash: true
-}));
-
-router.get('/users/logout', (req, res) => {
-    req.logout(() => {
-        req.flash('success_msg', 'You have logged out');
-        res.redirect('/users/login');
-    });
-});
-
-router.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
-    res.render("dashboard", { user: req.user.name });
-});
-
-// Middleware functions to check if user is authenticated
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect("/users/dashboard");
-    }
-    next();
 }
 
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/users/login");
-}
-
-module.exports = router;
+module.exports = { register };
