30b9506aff7625ad3243429f7597a913ce829d81
Roman Shrestha <stha.roman20@gmail.com>
2024-05-04 10:39:29 +0300

    commited

--- server/helpers/transporter.js
+++ server/helpers/transporter.js
@@ -1,45 +1,38 @@
-const crypto = require('crypto');
 const nodemailer = require('nodemailer');
-require('dotenv').config();
-const { pool } = require('./db');
+const crypto = require('crypto');
 
-// function to generate reset token
+// Function to generate reset token
 function generateResetToken() {
     return crypto.randomBytes(20).toString('hex');
 }
 
-// function to send reset password email confirmation
-async function sendResetEmail(email, token, req){
-    try{
-
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true, // true for 465, false for other ports
+// Function to send reset email
+async function sendResetEmail(email, token, req) {
+    try {
+        // Configure Nodemailer transporter
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
             auth: {
-                user: process.env.EMAIL, // generated ethereal user
-                pass: process.env.PASSWORD // generated ethereal password
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
             }
         });
-        
-        // send mail with defined transport object
+
+        // Send email with reset link
         await transporter.sendMail({
-            to: email, // list of receivers
-            from: process.env.EMAIL, // sender address
-            subject: 'Password Reset Request', // Subject line
-            text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
-            Please click on the following link, or paste this into your browser to complete the process:\n\n
-            http://${req.headers.host}/users/reset-password/${token}\n\n
-            If you did not request this, please ignore this email and your password will remain unchanged.\n` // plain text body
+            to: email,
+            from: 'stha.roman20@gmail.com',
+            subject: 'Password Reset Request',
+            text: `Hello, You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+                Please click on the following link, or paste this into your browser to complete the process:\n\n
+                http://${req.headers.host}/users/reset-password/${token}\n\n
+                If you did not request this, please ignore this email and your password will remain unchanged.\n`
         });
     } catch (error) {
-        console.error('Error sending email:', error);
-        throw new Error('Error sending email');
+        console.error(error);
+        throw new Error('Error sending email.');
     }
 }
-    
-    module.exports = { 
-    sendResetEmail,
-    generateResetToken
-};
+
+
+module.exports = { generateResetToken, sendResetEmail };
